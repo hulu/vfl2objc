d3a2b1af9f1b5c6132f9cc610819e98d98c4cd2c
Bao Lei <bao.lei@hulu.com>
2013-04-28 21:45:30 -0700

    use symbolic link

--- setup.rb
+++ setup.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-`ln -f #{File.expand_path(File.dirname(__FILE__))}/vfl2objc.rb /usr/bin/`
+`ln -sf #{File.expand_path(File.dirname(__FILE__))}/vfl2objc.rb /usr/bin/`
 `rm -rf ~/Library/Services/vfl-file.workflow`
 `cp -r #{File.expand_path(File.dirname(__FILE__))}/vfl-file.workflow ~/Library/Services/`
 
